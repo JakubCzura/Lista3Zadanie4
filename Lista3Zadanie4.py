545610f10b6e27d34d64366c215c1fbd1eb68b25
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-04-24 22:37:10 +0200

    Temperature in progress

--- Lista3Zadanie4.py
+++ Lista3Zadanie4.py
@@ -1,21 +1,50 @@
 import abc
 
 class Temperature(abc.ABC):
-    def __init__(self, temperature)
 
-    temperature:float = 0
+    @abc.abstractclassmethod
+    def __init__(self, temperature):
+        self.temperature = temperature
 
-    __str__():
+    @classmethod
+    def __str__(self):
+        return f"{self.temperature} stopni w skali Celsjusz"
 
-    __repr__():
+    @classmethod
+    def __repr__(self):
+         return self.__class__.__name__
+   
+    @property
+    def temperature(self):
+        return self.temperature
 
-    above_freezing():
+    @temperature.setter
+    def temperature(self, value):
+        self.temperature = value
+        #self.__dict__['temperature'] = value
 
-    convert_to_Fahrenheit():
 
-    convert_to_Celsius():
+    @classmethod
+    def above_freezing(self):
+        if self.temperature > 0:
+            return True
+        else:
+            return False
 
-    convert_to_Kelvin():
+    @abc.abstractclassmethod
+    def convert_to_Fahrenheit(self):
+        raise NotImplementedError
+
+
+    @abc.abstractclassmethod
+    def convert_to_Celsius(self):
+        raise NotImplementedError
+
+
+    @abc.abstractclassmethod
+    def convert_to_Kelvin(self):
+        raise NotImplementedError
+
 
 
 
