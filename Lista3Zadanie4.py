fe06be0df4c1ff26eece07f0e278231f214ce61e
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-04-24 21:34:24 +0200

    Temperature in progess

--- Lista3Zadanie4.py
+++ Lista3Zadanie4.py
@@ -1,1 +1,22 @@
+import abc
+
+class Temperature(abc.ABC):
+    def __init__(self, temperature)
+
+    temperature:float = 0
+
+    __str__():
+
+    __repr__():
+
+    above_freezing():
+
+    convert_to_Fahrenheit():
+
+    convert_to_Celsius():
+
+    convert_to_Kelvin():
+
+
+
 
