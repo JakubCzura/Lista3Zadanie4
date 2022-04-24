98ba33fee4089b1f411321af6909f4f19d70a689
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-04-25 01:46:26 +0200

    celsius, kelvin in progress

--- Celsius.py
+++ Celsius.py
@@ -1,7 +1,30 @@
 import Temperature
 
 class Celsius(Temperature.Temperature):
-    pass
+    def __init__(self, temperature):
+        super().__init__(self.temperature)
+
+    @property
+    def temperature(self):
+        return self.temperature
+
+    @temperature.setter
+    def temperature(self, value):
+        self.temperature = value
+
+    #@abc.abstractmethod
+    def convert_to_Fahrenheit(self):
+        temp = self.temperature/0.556 + 32.0
+        return Fahrenheit(self, temp)
+
+    #@abc.abstractmethod
+    def convert_to_Celsius(self):
+        return Celsius(self, self.temperature)
+    
+    #@abc.abstractmethod
+    def convert_to_Kelvin(self):
+        temp = self.temperature + 273.16
+        return Kelvin(self, temp)
 
 
 
--- Kelvin.py
+++ Kelvin.py
@@ -1,7 +1,30 @@
 import Temperature
 
 class Kelvin(Temperature.Temperature):
-    pass
+    def __init__(self, temperature):
+        super().__init__(self.temperature)
+
+    @property
+    def temperature(self):
+        return self.temperature
+
+    @temperature.setter
+    def temperature(self, value):
+        self.temperature = value
+
+    #@abc.abstractmethod
+    def convert_to_Fahrenheit(self):
+        temp = 1.8*(self.temperature-273) + 32
+        return Fahrenheit(self, temp)
+
+    #@abc.abstractmethod
+    def convert_to_Celsius(self):
+        temp = self.temperature - 273.16
+        return Celsius(self, temp)
+    
+    #@abc.abstractmethod
+    def convert_to_Kelvin(self):
+        return Kelvin(self, self.temperature)
 
 
 
